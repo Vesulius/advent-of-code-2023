98c8e54519e7085d71faac0e13baec7ef1c84fc8
vesa <vesa@pop-os.localdomain>
2023-12-27 19:04:43 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod day1;
+mod day1_2;
+mod day2;
+mod day2_2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+
+fn main() {
+    // day1::run();
+    // day1_2::run();
+    // day2::run();
+    // day2_2::run();
+    // day3::run();
+    // day4::run(true);
+    // day5::run(true);
+    day6::run(true);
+}
